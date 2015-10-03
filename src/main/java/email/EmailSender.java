055da8b1241444cbbaa9bc570137a3f7f7852978
Alex <warriorman_2006@ukr.net>
2015-10-03 19:59:35 +0300

    fix Email Sender

--- src/main/java/email/EmailSender.java
+++ src/main/java/email/EmailSender.java
@@ -5,46 +5,79 @@ import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import org.apache.log4j.Logger;
 
 public class EmailSender {
+
     private static final Logger logger = Logger.getLogger(EmailSender.class);
-    
-    Properties mailServerProperties;
-    Session getMailSession;
-    MimeMessage generateMailMessage;
+    Session mailSession;
 
     public void sendMessage(String to, String order) throws MessagingException {
-        
-        logger.debug("\n 1st ===> setup Mail Server Properties..");
-        mailServerProperties = System.getProperties();
-        mailServerProperties.put("mail.smtp.port", "465");
-        mailServerProperties.put("mail.smtp.auth", "true");
-        mailServerProperties.put("mail.smtp.starttls.enable", "true");
-        
-        logger.debug("Mail Server Properties have been setup successfully..");
+        setMailServerProperties();
+        draftEmailMessage();
+        sendEmail();
 
-        logger.debug("\n\n 2nd ===> get Mail Session..");
-        getMailSession = Session.getDefaultInstance(mailServerProperties, null);
-        generateMailMessage = new MimeMessage(getMailSession);
-        generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress("warriorman_2006@ukr.net"));
-        generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress("warriorman_2006@ukr.net"));
-        generateMailMessage.setSubject(order);
-        String emailBody = "Test email by Crunchify.com JavaMail API example. " + "<br><br> Regards, <br>Crunchify Admin";
-        generateMailMessage.setContent(emailBody, "text/html");
-        logger.debug("Mail Session has been created successfully..");
-        
-        logger.debug("\n\n 3rd ===> Get Session and Send mail");
-        Transport transport = getMailSession.getTransport("smtp");
+    }
 
-		// Enter your correct gmail UserID and Password
-        // if you have 2FA enabled then provide App Specific Password
-        transport.connect("smtp.ukr.net", "warriorman_2006@ukr.net", "45s84as7");
-        transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+    private void setMailServerProperties() {
+        Properties emailProperties = System.getProperties();
+        emailProperties.put("mail.smtp.port", "586");
+        emailProperties.put("mail.smtp.auth", "true");
+        emailProperties.put("mail.smtp.starttls.enable", "true");
+        mailSession = Session.getDefaultInstance(emailProperties, null);
+    }
+
+    private MimeMessage draftEmailMessage() throws AddressException, MessagingException {
+        String to = "warriorman_2006@ukr.net";
+        String emailSubject = "Test email subject";
+        String emailBody = "Test";
+        MimeMessage emailMessage = new MimeMessage(mailSession);
+        /**
+         * Set the mail recipients
+         *
+         */
+        
+        emailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        
+        emailMessage.setSubject(emailSubject);
+        /**
+         * If sending HTML mail
+         *
+         */
+        emailMessage.setContent(emailBody, "text/html");
+        /**
+         * If sending only text mail
+         *
+         */
+        //emailMessage.setText(emailBody);// for a text email
+        return emailMessage;
+    }
+
+    private void sendEmail() throws AddressException, MessagingException {
+        /**
+         * Sender's credentials
+         *
+         */
+        String fromUser = "openshift.onlinestore@gmail.com";
+        String fromUserEmailPassword = "UAorders";
+
+        String emailHost = "smtp.gmail.com";
+        Transport transport = mailSession.getTransport("smtp");
+        transport.connect(emailHost, fromUser, fromUserEmailPassword);
+        /**
+         * Draft the message
+         *
+         */
+        MimeMessage emailMessage = draftEmailMessage();
+        /**
+         * Send the mail
+         *
+         */
+        transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
         transport.close();
+        logger.debug("Email sent successfully.");
     }
 }
-
-
