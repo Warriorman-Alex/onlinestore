61de49c3ebffc3afaa26e70673bfa1f102aec050
Alex <warriorman_2006@ukr.net>
2015-10-03 14:21:40 +0300

    Changed port

--- src/main/java/email/EmailSender.java
+++ src/main/java/email/EmailSender.java
@@ -20,7 +20,7 @@ public class EmailSender {
         
         logger.debug("\n 1st ===> setup Mail Server Properties..");
         mailServerProperties = System.getProperties();
-        mailServerProperties.put("mail.smtp.port", "587");
+        mailServerProperties.put("mail.smtp.port", "465");
         mailServerProperties.put("mail.smtp.auth", "true");
         mailServerProperties.put("mail.smtp.starttls.enable", "true");
         mailServerProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
